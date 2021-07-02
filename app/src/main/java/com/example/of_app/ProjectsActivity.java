e2874ff5bd106cec46499cee5b910b61e0804a2a
damprog <damprog.one@gmail.com>
2021-07-02 12:52:03 +0200

    Pre-prepared structure of the application x2

--- app/src/main/java/com/example/of_app/ProjectsActivity.java
+++ app/src/main/java/com/example/of_app/ProjectsActivity.java
@@ -1,17 +1,34 @@
 package com.example.of_app;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ProjectsActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> adapter;
+    private ListView projectsListLV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects);
+
+        projectsListLV = findViewById(R.id.projectsListLV);
+        String[] st = {"projekt1", "projekt2", "projekt3"};
+        ArrayList<String> pList = new ArrayList<>();
+        pList.addAll(Arrays.asList(st));
+        adapter = new ArrayAdapter<String>(this, R.layout.activity_projects, pList);
+        projectsListLV.setAdapter(adapter);
 
         final Button newFormBtn = findViewById(R.id.newFormBtn);
         newFormBtn.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +41,7 @@ public class ProjectsActivity extends AppCompatActivity {
         });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
     }
 }
